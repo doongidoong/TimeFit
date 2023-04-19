7bf6c2c51168405838be30453fd928d1eefa9779
Park Sangyeop <meko6970@gmail.com>
2023-04-19 21:11:04 +0900

    Refactor : 예약 기능 구현 Redisson 분산락 사용

--- server/src/main/java/com/project/service/ReservationService.java
+++ server/src/main/java/com/project/service/ReservationService.java
@@ -17,11 +17,15 @@ import com.project.response.ReservationResponse;
 import com.project.response.ReservationUserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.RedissonLock;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Transactional
@@ -33,10 +37,12 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final CenterRepository centerRepository;
     private final CenterEquipmentRepository centerEquipmentRepository;
+    private final RedissonClient redissonClient;
     // 예약 정보를 갖는 Map 초기화
     private Map<Long, Queue<Long>> reserveQueue = new ConcurrentHashMap<>();
 
     // 헬스장 기구의 예약 내역 조회
+    @Transactional(readOnly = true)
     public List<ReservationResponse> getReservation(Long id, ReservationSearch request){
         List<ReservationResponse> reservationList = new ArrayList<>();
         // 받은 헬스장 기구 ID 리스트를 돌면서 예약 내역 리스트 구성
@@ -57,9 +63,9 @@ public class ReservationService {
         //예약 있을 시 예외처리
 
         //예약이 있는지 DB에 조회 후 예약이 있다면 있을 시 예외처리
-//        if(!reservationRepository.check(centerId, request)){
-//            throw new ReservationExist();
-//        }
+        if(!reservationRepository.check(centerId, request)){
+            throw new ReservationExist();
+        }
 
         // 예약 정보를 만들기 위해 센터 id와 기구 id로부터 정보를 불러옴
         Center center = centerRepository.findById(centerId)
@@ -98,10 +104,30 @@ public class ReservationService {
         requestReservation(centerId, request, user);
         reserveQueue.get(request.getCenterEquipmentId()).remove(threadId); // 예약 요청 정보를 내보냄
     }
+    //분산락을 통한 예약 처리
+    public void requestReservationByLock(Long centerId, ReservationRequest request, User user){
+        RLock lock = redissonClient.getFairLock(String.valueOf(request.getCenterEquipmentId()));
+        try{
+            boolean available = lock.tryLock(5, 1, TimeUnit.SECONDS);
+            Thread.sleep(1000);
+            if (!available) {
+                log.info("lock 획득 실패");
+                return;
+            }
+            requestReservation(centerId, request, user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(lock != null && lock.isLocked()){
+                lock.unlock();
+            }
+        }
+    }
     // 예약 ID를 통해 예약 취소
     public void cancelReservation(Long reservationId) {
         reservationRepository.deleteById(reservationId);
     }
+    @Transactional(readOnly = true)
     // User 정보를 통해 예약 정보를 불러와 List로 반환
     public List<ReservationUserResponse> getMyReservation(User user){
         return reservationRepository.getMyReserve(user).stream()
@@ -109,9 +135,30 @@ public class ReservationService {
                 .collect(Collectors.toList());
     }
 
-
-
-
+    public Long lockTest(Long lockId){
+        // 파라미터로 넘어온 Id로 Lock 획득
+        RLock lock = redissonClient.getFairLock(String.valueOf(lockId));
+        long before = System.currentTimeMillis();
+        try{
+            boolean available = lock.tryLock(500, 100, TimeUnit.SECONDS);
+            if (!available) {
+                log.info("lock 획득 실패");
+                return null;
+            }
+            log.info("락 획득");
+            // 락 획득 후 1분간 대기
+            Thread.sleep(60000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(lock != null && lock.isLocked()){
+                log.info("=-----"+Thread.currentThread().getName());
+                lock.unlock();
+            }
+        }
+        long after = System.currentTimeMillis();
+        return after - before;
+    }
 
 
 }
--- server/src/test/java/com/project/service/ReservationServiceTest.java
+++ server/src/test/java/com/project/service/ReservationServiceTest.java
@@ -204,4 +204,52 @@ class ReservationServiceTest {
         Assertions.assertEquals(1,reservationRepository.findAll().size());
 
     }
+    @Test
+    void doubleBookingTestForRedis() throws Exception{
+        //given
+        User user= User.builder()
+                .email("id@naver.com")
+                .password("1234")
+                .name("이름")
+                .phoneNumber("010-2323-3333")
+                .birth("birth")
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build();
+        userRepository.save(user);
+        Center center = Center.builder()
+                .name("상품")
+                .build();
+        centerRepository.save(center);
+        CenterEquipment centerEquipment = CenterEquipment.builder()
+                .center(center)
+                .build();
+        centerEquipmentRepository.save(centerEquipment);
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        CountDownLatch latch = new CountDownLatch(5);
+        LocalDate now = LocalDate.now();
+        ReservationRequest request = ReservationRequest.builder()
+                .centerEquipmentId(centerEquipment.getId())
+                .start(LocalDateTime.parse(now+"T10:45:00"))
+                .end(LocalDateTime.parse(now+"T10:55:00"))
+                .build();
+        reservationService.addCenterInQueue(centerEquipment.getId());
+        //when
+        for (int i = 0; i < 5; i++) {
+            service.execute(()->{
+                try{
+                    reservationService.requestReservationByLock(center.getId(), request, user);
+                } catch (RuntimeException e) {
+                    Assertions.assertEquals("com.project.exception.ReservationExist: 예약 불가한 시간입니다."
+                            , e.getMessage());
+                    log.info("예약에 실패하였습니다.");
+                }
+                latch.countDown();
+            });
+        }latch.await();
+
+        //then
+        Assertions.assertEquals(1,reservationRepository.findAll().size());
+
+    }
 }
